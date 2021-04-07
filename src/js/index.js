056d206a3626cf9e2a2b7520dfd4ab6eaf9ad85e
Jesús Salazar Andrade <salazarandradejesus@gmail.com>
2021-04-07 10:58:49 -0400

    New Conditional Profile Generator Day 8

--- src/js/index.js
+++ src/js/index.js
@@ -6,7 +6,7 @@ import "../style/index.scss";
  * 
     {
         includeCover: true, // if includeCover is true the algorithm should
-        background: "https://images.unsplash.com/photo-1511974035430-5de47d3b95da", // this is the url of the image that will used as background for the profile cover
+        background: "https://tynmedia.com/tynmag/wp-content/uploads/sites/3/2020/05/tigo-business-300x160.jpg", // this is the url of the image that will used as background for the profile cover
         avatarURL: "https://randomuser.me/api/portraits/women/42.jpg", // this is the url for the profile avatar
         socialMediaPosition: "left", // social media bar position (left or right)
         
@@ -33,15 +33,32 @@ function render(variables = {}) {
   document.querySelector("#widget_content").innerHTML = `<div class="widget">
             ${cover}
           <img src="${variables.avatarURL}" class="photo" />
-          <h1>Lucy Boilett</h1>
-          <h2>Web Developer</h2>
-          <h3>Miami, USA</h3>
-          <ul class="position-right">
-            <li><a href="https://twitter.com/alesanchezr"><i class="fa fa-twitter"></i></a></li>
-            <li><a href="https://github.com/alesanchezr"><i class="fa fa-github"></i></a></li>
-            <li><a href="https://linkedin.com/alesanchezr"><i class="fa fa-linkedin"></i></a></li>
-            <li><a href="https://instagram.com/alesanchezr"><i class="fa fa-instagram"></i></a></li>
+          <h1>${variables.name ? variables.name : ""} ${
+    variables.lastname ? variables.lastname : ""
+  }
+    </h1>
+          <h2>${variables.role ? variables.role : ""}</h2>
+          <h3>${variables.city ? variables.city : ""}, ${
+    variables.country ? variables.country : ""
+  }</h3>
+          <ul class=${
+            variables.socialMediaPosition ? variables.socialMediaPosition : ""
+          }>
+            <li><a href="https://twitter.com/${
+              variables.twitter ? variables.twitter : ""
+            }"><i class="fa fa-twitter"></i></a></li>
+            <li><a href="https://github.com/${
+              variables.github ? variables.github : ""
+            }"><i class="fa fa-github"></i></a></li>
+            <li><a href="https://linkedin.com/in/${
+              variables.linkedin ? variables.linkedin : ""
+            }"><i class="fa fa-linkedin"></i></a></li>
+            <li><a href="https://instagram.com/${
+              variables.instagram ? variables.instagram : ""
+            }"><i class="fa fa-instagram"></i></a></li>
           </ul>
+          
+          
         </div>
     `;
 }
@@ -54,9 +71,11 @@ window.onload = function() {
     // if includeCover is true the algorithm should
     includeCover: true,
     // this is the url of the image that will used as background for the profile cover
-    background: "https://images.unsplash.com/photo-1511974035430-5de47d3b95da",
+    background:
+      "https://tynmedia.com/tynmag/wp-content/uploads/sites/3/2020/05/tigo-business-300x160.jpg",
     // this is the url for the profile avatar
-    avatarURL: "https://randomuser.me/api/portraits/women/42.jpg",
+    avatarURL:
+      "https://media.licdn.com/dms/image/C4E0BAQEZXQNSjKMZCQ/company-logo_200_200/0?e=2159024400&v=beta&t=Z_dGwyQSuxwLciCUkynfAoRnAoQcLdp-URIiXFW96Ks",
     // social media bar position (left or right)
     socialMediaPosition: "position-left",
     // social media usernames
